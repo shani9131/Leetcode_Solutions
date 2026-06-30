af2f1384ebb241374e02ee747754c09832f74a4d
Shani Kushwaha <shanikushwaha7400@gmail.com>
2026-06-30 15:03:21 +0530

    Time: 0 ms (100%), Space: 42.3 MB (12.77%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,27 @@
+class Solution {
+    Integer dp[][];
+
+    int uniquePaths(int m, int n) {
+        dp=new Integer[m+1][n+1];
+        return solve(0,0,m,n);
+
+    }
+    int solve(int i, int j, int m, int n){
+
+        if(i>=m || j>=n){
+            return 0;
+        }
+
+        if(i==m-1 || j==n-1){
+            return 1;
+        }
+        if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+
+        int right=solve(i,j+1,m,n);
+        int down=solve(i+1,j,m,n);
+        dp[i][j]=(right+down);
+        return dp[i][j];
+    }
+}
