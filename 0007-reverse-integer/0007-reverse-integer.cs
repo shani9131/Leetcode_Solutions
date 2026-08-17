f138094b630588a3b204a7f9cbf07ffca797f081
Shani Kushwaha <shanikushwaha7400@gmail.com>
2026-08-17 14:41:00 +0530

    Time: 12 ms (96.79%), Space: 28.9 MB (77.84%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cs
+++ 0007-reverse-integer/0007-reverse-integer.cs
@@ -2,14 +2,13 @@ public class Solution
 {
     public int Reverse(int x)
     {
-        var result = 0;
+        int result = 0;
 
         while (x != 0)
         {
-            var remainder = x % 10;
-            var temp = result * 10 + remainder;
+            int remainder = x % 10;
+            int temp = result * 10 + remainder;
 
-            // in case of overflow, the current value will not be equal to the previous one
             if ((temp - remainder) / 10 != result)
             {
                 return 0;
